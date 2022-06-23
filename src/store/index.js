00447f55a06016f28978ff59282ea7b720d9532c
Huiyu Li <curlyuu@gmail.com>
2022-06-23 17:03:46 +0800

    add vuex

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from "vuex";
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  getters: {
+    getCounter(state) {
+      return state.counter;
+    },
+  },
+  mutations: {
+    increment(state, payload) {
+      state.counter = state.counter + payload;
+    },
+  },
+  actions: {
+    increment(context, payload) {
+      context.commit("increment", payload);
+    },
+  },
+});
+
+export default store;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
 import TheWelcome from '@/components/TheWelcome.vue'
+
+const store = useStore();
+const count = computed(() => store.getters.getCounter);
+
+const countHandler = () => {
+  store.dispatch("increment",1);
+};
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <div class="container">
+      <h3>Count : {{ count }}</h3>
+      <button @click="countHandler">Count Handler</button>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.container{
+  margin-left: 6rem;
+}
+
+h3 {
+  font-size: 1.2rem;
+  font-weight: 500;
+  margin-bottom: 0.4rem;
+  color: var(--color-heading);
+}
+
+button{
+  background-color: var(--color-heading);
+  color: white;
+  padding: .75rem;
+  border: none;
+}
+</style>
